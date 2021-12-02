345ad4a49f7203e8f89ee5fa7cdc9832faade176
eraser5th <hisanniwo0202@gmail.com>
2021-12-03 01:46:05 +0900

    vercel config :

---
+++ api/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function handler(req, res) {
+    res.status(200).json({
+        body: req.body,
+        query: req.query,
+        cookies: req.cookies,
+    });
+}
+exports.default = handler;
