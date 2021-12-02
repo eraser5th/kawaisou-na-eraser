3605a4f252d3f2497e16cba291b8363ea8e46afc
eraser5th <hisanniwo0202@gmail.com>
2021-12-03 01:51:03 +0900

    a

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,6 @@
-export default function handler(req: any, res: any) {
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
   res.status(200).json({
     body: req.body,
     query: req.query,
