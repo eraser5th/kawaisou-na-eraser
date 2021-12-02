9c5dab65bdc7a4131c3b5515e24435260a9f0eec
eraser5th <hisanniwo0202@gmail.com>
2021-12-03 01:56:33 +0900

    b

--- api/index.js
+++ api/index.js
@@ -1,10 +1,9 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, '__esModule', { value: true });
 function handler(req, res) {
-    res.status(200).json({
-        body: req.body,
-        query: req.query,
-        cookies: req.cookies,
-    });
+  res.status(200).json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies,
+  });
 }
 exports.default = handler;
